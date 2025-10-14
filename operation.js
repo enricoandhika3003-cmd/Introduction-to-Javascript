bd33768e1512c3ed37d5ef69125c91fcceac80a1
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2025-10-14 19:12:33 +0700

    done 1

---
+++ operation.js
@@ -0,0 +1,22 @@
+console.log(9+10)
+
+var a = 12;
+console.log(A);
+
+let b = 13
+
+const c = 14
+
+var collection = ["Me, Myself, I"]
+var n = true
+var morningAlarm = "Wake Up!"
+
+console.log(2%2)
+console.log(2/2)
+
+var i = 5
+    i += 2
+    console.log(i)
+    i-=0 = i=i-0
+    i*=3 = i=i*3
+    i/=6 = i=i/6
